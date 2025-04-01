e96e9c43ed3eaa672a0ad6c70fbc5989bdf9ba75
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2025-04-02 00:28:17 +0800

    Update MergeSort.cpp

--- Hw1/MergeSort.cpp
+++ Hw1/MergeSort.cpp
@@ -90,9 +90,9 @@ int main() {
     }
 
     if (n > 1000) {
-        cout << "排序後前10個元素: ";
+        cout << "排序後前15個元素: ";
         for (int i = 0; i < 10 && i < arr.size(); ++i) {
-            cout << arr[i] << " ";
+            cout << arr[i] << "\n ";
         }
     }
 
