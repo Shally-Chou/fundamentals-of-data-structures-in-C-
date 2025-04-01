b34666cab76282c248566cdce00f1edce61fe8ee
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2025-04-02 00:31:08 +0800

    Update QuickSort.cpp

--- Hw1/QuickSort.cpp
+++ Hw1/QuickSort.cpp
@@ -86,7 +86,7 @@ int main() {
     if (n > 1000) {
         cout << "排序後前15個元素: ";
         for (int i = 0; i < 10 && i < arr.size(); ++i) {
-            cout << arr[i] << "\n ";
+            cout << "\n" << arr[i] << " ";
         }
         cout << endl;
     }
