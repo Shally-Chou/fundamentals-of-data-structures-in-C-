e926f7a8d9783c9254a8ccc62918837bbe722278
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2025-05-10 21:44:15 +0800

    Update random-graph.cpp

--- Hw2/random-graph.cpp
+++ Hw2/random-graph.cpp
@@ -9,90 +9,90 @@
 using namespace std;
 
 // 產生graph
-void generateRandomGraph(int n, int e, vector<vector<int>>& adjMatrix, vector<vector<int>>& adjList) {
-    srand(time(0));
-    set<pair<int, int>> edgeSet;
+void generateRandomGraph(int n, int e, vector<vector<int>>& adjMatrix, vector<vector<int>>& adjList) {    //
+    srand(time(0));    //
+    set<pair<int, int>> edgeSet;    //
 
     while ((int)edgeSet.size() < e) {
-        int u = rand() % n;
+        int u = rand() % n;    //設vertex
         int v = rand() % n;
-        if (u != v) {
-            int a = min(u, v), b = max(u, v);
-            if (edgeSet.count({a, b}) == 0) {
-                edgeSet.insert({a, b});
-                adjMatrix[a][b] = adjMatrix[b][a] = 1;
-                adjList[a].push_back(b);
-                adjList[b].push_back(a);
+        if (u != v) {    //確保不是自己走到自己
+            int a = min(u, v), b = max(u, v);    //
+            if (edgeSet.count({a, b}) == 0) {    //如果這條路還沒被記錄到
+                edgeSet.insert({a, b});    //加進去
+                adjMatrix[a][b] = adjMatrix[b][a] = 1;    //設矩陣 a,b=b,a 無向圖 長度一律設為1
+                adjList[a].push_back(b);    //
+                adjList[b].push_back(a);    //
             }
         }
     }
 }
 
 // 匯出CSV file
-void exportEdgeList(const vector<vector<int>>& graph, const string& filename) {
+void exportEdgeList(const vector<vector<int>>& graph, const string& filename) {    //
     ofstream file(filename);
-    file << "Vertex 1,Vertex 2\n";
-    int n = graph.size();
-    for (int u = 0; u < n; ++u) {
-        for (int v : graph[u]) {
-            if (u < v) file << u << "," << v << "\n";
+    file << "Vertex 1,Vertex 2\n";    //寫入file的第一行
+    int n = graph.size();    //
+    for (int u = 0; u < n; ++u) {    //歷遍串列
+        for (int v : graph[u]) {    //
+            if (u < v) file << u << "," << v << "\n";    //避免邊出現重複
         }
     }
     file.close();
 }
 
 // DFS 遍歷
-void dfs(int u, vector<bool>& visited, const vector<vector<int>>& graph, vector<vector<int>>& tree) {
-    visited[u] = true;
-    for (int v : graph[u]) {
-        if (!visited[v]) {
-            tree[u].push_back(v);
-            tree[v].push_back(u);
-            dfs(v, visited, graph, tree);
+void dfs(int u, vector<bool>& visited, const vector<vector<int>>& graph, vector<vector<int>>& tree) {    //
+    visited[u] = true;    //標記為走過 grey
+    for (int v : graph[u]) {    //
+        if (!visited[v]) {    //如果連到的點有沒走過的 white
+            tree[u].push_back(v);    //
+            tree[v].push_back(u);    //
+            dfs(v, visited, graph, tree);    //重跑dfs u變成v
         }
     }
 }
 
 // BFS 遍歷
-void bfs(int start, vector<bool>& visited, const vector<vector<int>>& graph, vector<vector<int>>& tree) {
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
+void bfs(int start, vector<bool>& visited, const vector<vector<int>>& graph, vector<vector<int>>& tree) {    //
+    queue<int> q;    //建一個列 放等等要尋訪的東西 像是看1把2,3放進去 看2序列變成3,4,5
+    q.push(start);    //把起始點放到列裡面 top是1就把1放進去
+    visited[start] = true;    //標記為走過 grey
 
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
+    while (!q.empty()) {    //當列不為空時就一直跑
+        int u = q.front(); q.pop();    //最前面的值給u 然後把他從列中刪掉
         for (int v : graph[u]) {
-            if (!visited[v]) {
-                visited[v] = true;
-                tree[u].push_back(v);
-                tree[v].push_back(u);
-                q.push(v);
+            if (!visited[v]) {    //如果連到的點有沒走過的 white
+                visited[v] = true;    //過去 標為走過 grey
+                tree[u].push_back(v);    //
+                tree[v].push_back(u);    //
+                q.push(v);    //
             }
         }
     }
 }
 
 // 印出鄰接矩陣
-void printAdjMatrix(const vector<vector<int>>& matrix) {
+void printAdjMatrix(const vector<vector<int>>& matrix) {    //
     int n = matrix.size();
     cout << "    ";
-    for (int i = 0; i < n; ++i) cout << i << "  ";
+    for (int i = 0; i < n; ++i) cout << i << "  ";    //有幾個數字就印到多少 阿從0開始
     cout << endl;
     for (int i = 0; i < n; ++i) {
         cout << " " << i << " ";
         for (int j = 0; j < n; ++j) {
-            cout << " " << matrix[i][j] << " ";
+            cout << " " << matrix[i][j] << " ";    //
         }
         cout << endl;
     }
 }
 
 // 印出鄰接串列
-void printAdjList(const vector<vector<int>>& adjList) {
-    for (int i = 0; i < adjList.size(); ++i) {
+void printAdjList(const vector<vector<int>>& adjList) {    //
+    for (int i = 0; i < adjList.size(); ++i) {    //
         cout << i << ": ";
-        for (int j : adjList[i]) cout << j << " ";
-        cout << endl;
+        for (int j : adjList[i]) cout << j << " ";    //
+        cout << endl;    //
     }
 }
 
@@ -106,35 +106,35 @@ int main() {
 
     if (e > maxEdge) {
         cout << "邊數超過最大可能值，請重新輸入。" << endl;
-        return 0;
+        return 0;    //
     }
 
-    vector<vector<int>> adjMatrix(n, vector<int>(n, 0));
-    vector<vector<int>> adjList(n);
-    generateRandomGraph(n, e, adjMatrix, adjList);
+    vector<vector<int>> adjMatrix(n, vector<int>(n, 0));    //
+    vector<vector<int>> adjList(n);    //
+    generateRandomGraph(n, e, adjMatrix, adjList);    //
 
-    cout << "\n鄰接矩陣:" << endl;
-    printAdjMatrix(adjMatrix);
+    cout << "\n鄰接矩陣 :" << endl;
+    printAdjMatrix(adjMatrix);    //
 
-    cout << "\n鄰接串列:" << endl;
-    printAdjList(adjList);
+    cout << "\n鄰接串列 :" << endl;
+    printAdjList(adjList);    //
 
     // 匯出graph
-    exportEdgeList(adjList, "graph_edges.csv");
+    exportEdgeList(adjList, "graph_edges.csv");    //
 
     // DFS Tree
-    vector<vector<int>> dfs_tree(n);
-    vector<bool> visited(n, false);
+    vector<vector<int>> dfs_tree(n);    //
+    vector<bool> visited(n, false);    //
     for (int i = 0; i < n; ++i)
-        if (!visited[i]) dfs(i, visited, adjList, dfs_tree);
-    exportEdgeList(dfs_tree, "dfs_tree_edges.csv");
+        if (!visited[i]) dfs(i, visited, adjList, dfs_tree);    //
+    exportEdgeList(dfs_tree, "dfs_tree_edges.csv");    //
 
     // BFS Tree
     vector<vector<int>> bfs_tree(n);
-    fill(visited.begin(), visited.end(), false);
+    fill(visited.begin(), visited.end(), false);    //
     for (int i = 0; i < n; ++i)
-        if (!visited[i]) bfs(i, visited, adjList, bfs_tree);
-    exportEdgeList(bfs_tree, "bfs_tree_edges.csv");
+        if (!visited[i]) bfs(i, visited, adjList, bfs_tree);    //
+    exportEdgeList(bfs_tree, "bfs_tree_edges.csv");    //
 
     // 印出 DFS/BFS Tree 的鄰接串列
     cout << "\nDFS Tree 的鄰接串列:" << endl;
